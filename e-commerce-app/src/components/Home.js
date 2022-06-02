07965ed4cd2251baf7b1b33d235bb90269c59e68
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-06-01 19:16:49 -0700

    add home page backdrop

--- e-commerce-app/src/components/Home.js
+++ e-commerce-app/src/components/Home.js
@@ -1,12 +1,18 @@
-import React from 'react'
-import './Home.css'
+import React from "react";
+import "./Home.css";
+import backdrop from "../icons/backdrop.png";
 
 function Home() {
   return (
     <div className="home">
-      Home
+      <div className="home__container">
+        <img src={backdrop} className="home__image" alt="backdrop" />
+        <div className="home__row">
+          
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
