94f5635de079f9acde4b7126369c5c135c30797c
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-06-01 18:53:34 -0700

    finish nav bar though hardcoded data

--- e-commerce-app/src/components/Header.js
+++ e-commerce-app/src/components/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Header.css";
 import siteicon from "../icons/siteicon.png";
 import SearchIcon from "@mui/icons-material/Search";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
 
 function Header() {
   return (
@@ -9,7 +10,7 @@ function Header() {
       <img src={siteicon} className="header__logo" alt="icon" />
       <div className="header__search">
         <input type="text" className="header__searchInput" />
-        <SearchIcon className="header__searchIcon"/>
+        <SearchIcon className="header__searchIcon" />
       </div>
       <div className="header__nav">
         <div className="header__option">
@@ -23,6 +24,10 @@ function Header() {
         <div className="header__option">
           <span className="header__optionLineOne">Your</span>
           <span className="header__optionLineTwo">Prime</span>
+        </div>
+        <div className="header__optionBasket">
+          <ShoppingBasketIcon />
+          <span className="header__optionLineTwo header__basketCount">0</span>
         </div>
       </div>
     </div>
