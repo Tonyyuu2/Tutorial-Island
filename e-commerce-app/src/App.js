b2dd6ae2062695235113f0bc402a65175e09aebe
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-06-02 17:00:48 -0700

    add react router

--- e-commerce-app/src/App.js
+++ e-commerce-app/src/App.js
@@ -1,12 +1,15 @@
-import './App.css';
-import Header from './components/Header'
-import Home from './components/Home'
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Home />
+      <Routes>
+        <Route path='/' element={ <Home /> } />
+      </Routes>
     </div>
   );
 }
